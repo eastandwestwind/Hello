e30d3ff76b0b66b102c675c7c381ebb406f1fe94
Catherine Smith <eastandwestwind@gmail.com>
2016-07-04 10:32:39 -0400

    add arcade physics

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 
+var game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update, render:render});
 
-
-var game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create });
-
-
+var stick;
+var jumpTimer = 0;
+var cursors;
+var jumpButton;
+var facing = 'idle';
 
 function preload() {
     //  2250 x 188 size of spritesheet
@@ -25,9 +27,16 @@ function preload() {
 function create() {
     game.stage.backgroundColor = "#ffffff";
 
-    var stick = game.add.sprite(game.width / 2, 500, 'stick');
+    stick = game.add.sprite(game.width / 2, 500, 'stick');
 
     stick.anchor.setTo(.5, 0);
+
+    // enable arcade physics
+    game.physics.enable(stick, Phaser.Physics.ARCADE);
+
+    game.physics.arcade.gravity.y = 1000;
+    stick.body.bounce.y = 0.2;
+    stick.body.collideWorldBounds = true;
 
     //  Here we add a new animation called 'walk'
     //  Because we didn't give any other parameters it's going to make an animation from all available frames in the 'mummy' sprite sheet
@@ -36,64 +45,62 @@ function create() {
     var crouch = stick.animations.add('crouch',[0]);
     var jump = stick.animations.add('jump',[1]);
 
+    cursors = game.input.keyboard.createCursorKeys();
+    jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+}
 
-    //  And this starts the animation playing by using its key ("walk")
-    //  30 is the frame rate (30fps)
-    //  true means it will loop when it finishes
-    stick.animations.play('stand', 4, true);
+function update() {
 
-    var keysdown = {};
+    // game.physics.arcade.collide(player, layer);
 
-    // keydown handler
-    $(document).keydown(function(e){
+    stick.body.velocity.x = 0;
 
-      // Do we already know it's down?
-      if (keysdown[e.which]) {
-          // Ignore it
-          return;
-      }
-
-      // Remember it's down
-      keysdown[e.which] = true;
-
-      // Do our thing
-      switch(e.which){
-        case 37: //left arrow
-
-          console.log('left');
-          stick.scale.x = -1;
-          stick.animations.play('walk', 7, true);
-          break;
-
-        case 38: //up arrow
-
-          console.log('up');
-          stick.animations.play('jump', 1, true);
-          break;
-
-        case 39: //right arrow
-
-          console.log('right');
-          stick.animations.play('walk', 7, true);
-          break;
-
-        case 40: //down arrow
-
-          console.log('down');
-          stick.animations.play('crouch', 1, true);
-          break;
-
-      }
-      e.preventDefault();
-    });
-
-    // keyup handler
-    $(document).keyup(function(e){
-      // Remove this key from the map
-      delete keysdown[e.which];
+    if (cursors.left.isDown)
+    {
+      stick.body.velocity.x=-150;
+      if (facing != 'left')
+       {
+           stick.scale.x = -1;
+           stick.animations.play('walk', 9, true);
+           facing = 'left';
+       }
+    }
+    else if (cursors.right.isDown)
+    {
+      stick.body.velocity.x=150;
+      if (facing != 'right')
+        {
+            stick.scale.x = 1;
+            stick.animations.play('walk', 9, true);
+            facing = 'right';
+        }
+    }
+    else
+    {
+      facing = 'idle'
       // default to standing animation
       stick.animations.play('stand', 4, true);
       // return scale
       stick.scale.x = 1;
-    });
+      // set velocity to 0
+      stick.body.velocity.x=0;
+    }
+
+    if (jumpButton.isDown && stick.body.onFloor() && game.time.now > jumpTimer)
+    {
+      if (facing != 'left')
+        {
+          stick.scale.x = 1;
+        }
+      else {
+          stick.scale.x = -1;
+      }
+      stick.animations.play('jump', 4, true);
+      stick.body.velocity.y = -250;
+      jumpTimer = game.time.now + 750;
+    }
+}
+
+function render () {
+  game.debug.text(game.time.suggestedFps, 32, 32);
 }
